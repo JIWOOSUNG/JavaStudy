7425725e48ddf8120a0dbbfd5dcbad393a187ad3
woosu <woosung727@naver.com>
2024-04-09 12:01:56 +0900

    1일차 문제해결 - Q1000 - Q1001 - Q2557 - Q10718 배운점: psvm 단축키, sout단축키, Scanner쓰는법

---
+++ src/Day1/Q1001.java
@@ -0,0 +1,15 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Q1001 {
+    //main함수 필요 psvm
+    public static void main(String[] args) {
+        //입력이 필요: 스캐너 사용
+        Scanner sc = new Scanner(System.in);//객체 새로 만들때 new, 입력받기때문에 System.in
+        int A = sc.nextInt(); // 스캐너에서 nextInt로 받음
+        int B = sc.nextInt();
+        //출력은 a+b
+        System.out.println(A-B);
+    }
+}
---
+++ src/Day1/Q2557.java
@@ -0,0 +1,8 @@
+package Day1;
+
+public class Q2557 {
+    public static void main(String[] args) {//메인함수 만드는 단축키 psvm + 엔터 (p만 쳐도 됨)
+        //출력하는 것은 sout + 엔터(s만 쳐도 됨)
+        System.out.println("Hello World!");
+    }
+}
