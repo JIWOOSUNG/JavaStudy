9156671152383d709d09ede4acc5a97192af0662
wsung <woosung727@naver.com>
2024-04-12 17:12:29 +0900

    4일차 문제해결

---
+++ src/Day4/Q10171.java
@@ -0,0 +1,11 @@
+package Day4;
+
+public class Q10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");   //\    /\
+        System.out.println(" )  ( \')");   // )  ( ')
+        System.out.println("(  /  )");     //(  /  )
+        System.out.println(" \\(__)|");            // \(__)|
+
+    }
+}
---
+++ src/Day4/Q10172.java
@@ -0,0 +1,11 @@
+package Day4;
+
+public class Q10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");         //|\_/|
+        System.out.println("|q p|   /}");      //|q p|   /}
+        System.out.println("( 0 )\"\"\"\\");  //( 0 )"""\
+        System.out.println("|\"^\"`    |");  //|"^"`    |
+        System.out.println("||_/=\\\\__|");   //||_/=\\__|
+    }
+}
