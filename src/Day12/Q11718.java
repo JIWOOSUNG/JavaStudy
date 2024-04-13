94606d5348fcaf56754df7231cfcab1d9a6a0f2a
wsung <woosung727@naver.com>
2024-04-14 02:48:14 +0900

    11일차 12일차 문제해결

---
+++ src/Day12/Q11718.java
@@ -0,0 +1,14 @@
+package Day12;
+
+import java.util.Scanner;
+
+public class Q11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()){
+            String str = sc.nextLine(); // next:스 페이스바 이전값을 출력 nextline: 엔터 이전값을 출력
+            System.out.println(str);
+        }
+    }
+}
---
+++ src/Day12/Q7287.java
@@ -0,0 +1,8 @@
+package Day12;
+
+public class Q7287 {
+    public static void main(String[] args) {
+        System.out.println(29);
+        System.out.println("woosung727");
+    }
+}
